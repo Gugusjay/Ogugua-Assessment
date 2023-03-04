a775d1c41239d56e9b842f6960811ecbc967ffec
OGUGUA EZEDOZIE <o.ezedozie.22@abdn.ac.uk>
2023-03-04 15:33:46 +0000

    last

--- exoplanets.py
+++ exoplanets.py
@@ -39,19 +39,4 @@ def planets():
     # Render the planets template with the data
     return render_template('planets.html', rows=rows)
 
-# from flask import Flask, jsonify
 
-app = Flask(__name__)
-
-# Application routes go here
-
-@app.errorhandler(404)
-def not_found(error):
-    return jsonify({'error': 'Not found'}), 404
-
-@app.errorhandler(500)
-def internal_server_error(error):
-    return jsonify({'error': 'Internal server error'}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
